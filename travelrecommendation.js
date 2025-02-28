fff6d9821e099cc363c86c7f82d4bd0e3a937956
anouar garbaa <g.anouar@yahoo.com>
2025-02-28 15:09:11 +0100

    add the search functionality

--- travelrecommendation.js
+++ travelrecommendation.js
@@ -8,39 +8,33 @@ let query = document.getElementById("searchinput");
 const clearsearch = () => {
   query.value = "";
   mydiv.style.display = "none";
-  console.log("Clearing");
+  result.innerHTML = "";
 };
 
 clearbtn.addEventListener("click", clearsearch);
 
 const showResult = (name, img, info) => {
-  if (mydiv.style.display === "none" || mydiv.style.display === "") {
-    mydiv.style.display = "block";
-  } else {
-    mydiv.style.display = "none";
-  }
-  result.innerHTML = `
-    <h2 class="title">${name}</h2>
-    <img class="search-img" src=${img} alt="sofia">
-    <p class="description">${info}</p>
+  mydiv.style.display = "block";
+  result.innerHTML += `
+    <div class="search-result-item">
+      <h2 class="title">${name}</h2>
+      <img class="search-img" src=${img} alt="${name}">
+      <p class="description">${info}</p>
+    </div>
   `;
 };
 
 const closeDropdown = () => {
   mydiv.style.display = "none";
   query.value = "";
+  result.innerHTML = "";
 };
 
 close.addEventListener("click", closeDropdown);
 
 const searchError = () => {
-  if (mydiv.style.display === "none" || mydiv.style.display === "") {
-    mydiv.style.display = "block";
-  } else {
-    mydiv.style.display = "none";
-  }
-
-  result.innerHTML = `<p class="notfound">Sorry we can't find your search</p>`;
+  mydiv.style.display = "block";
+  result.innerHTML = `<p class="notfound">Sorry, we can't find your search</p>`;
 };
 
 fetch("travelrecommendation.json")
@@ -49,26 +43,18 @@ fetch("travelrecommendation.json")
     const search = () => {
       let searchQuery = query.value.toLowerCase();
       let notfound = true;
+      result.innerHTML = ""; // Clear previous results
 
-      data.countries.map((country) => {
-        country.cities.map((city) => {
-          if (city.name.toLowerCase().includes(searchQuery)) {
-            showResult(city.name, city.imageUrl, city.description);
-            notfound = false;
-          }
-        });
-      });
-
-      data.temples.map((temple) => {
-        if (temple.name.toLowerCase().includes(searchQuery)) {
-          showResult(temple.name, temple.imageUrl, temple.description);
-          notfound = false;
-        }
-      });
-
-      data.beaches.map((beach) => {
-        if (beach.name.toLowerCase().includes(searchQuery)) {
-          showResult(beach.name, beach.imageUrl, beach.description);
+      [...data.countries, ...data.temples, ...data.beaches].forEach((item) => {
+        if (item.cities) {
+          item.cities.forEach((city) => {
+            if (city.name.toLowerCase().includes(searchQuery)) {
+              showResult(city.name, city.imageUrl, city.description);
+              notfound = false;
+            }
+          });
+        } else if (item.name.toLowerCase().includes(searchQuery)) {
+          showResult(item.name, item.imageUrl, item.description);
           notfound = false;
         }
       });
@@ -79,4 +65,9 @@ fetch("travelrecommendation.json")
     };
 
     searchbtn.addEventListener("click", search);
+    query.addEventListener("keyup", (event) => {
+      if (event.key === "Enter") {
+        search();
+      }
+    });
   });
